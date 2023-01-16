ff4442faaea6cc3ae87b99317d456c46244bd37a
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-01-16 15:17:47 +0900

    대소문자 변환

---
+++ src/test1.java
@@ -0,0 +1,11 @@
+public class test1 {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        char ch2 = 65;
+        short ch3 = 65;
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println(ch3);
+    }
+}
